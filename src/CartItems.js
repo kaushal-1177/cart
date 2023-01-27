8e7f1b1a633af654ed31bbd22db38472c9ce1a30
kaushal-1177 <kaushalkhurana17@gmail.com>
2023-01-27 12:47:12 +0530

    delete product from cart

--- src/CartItems.js
+++ src/CartItems.js
@@ -1,90 +1,54 @@
 import React from 'react';
-class CartItem extends React.Component{
-    constructor () {
-        super();
-            this.state = {
-                price: 999,
-                title: 'Mobile Phone',
-                qty: 13,
-                img: ''
-            }
-            this.increaseQuantity = this.increaseQuantity.bind(this);
-            this.testing();
-        }
 
-        testing() {
-            const promise = new Promise((resolve, reject) => {
-                setTimeout(() => {
-                    resolve('done');
-                }, 5000)
-            })
-
-            promise.then(() => {
-                // setState acts like a synchrons call
-                this.setState({qty: this.state.qty + 10});
-                console.log('state', this.state);
-            })
-        }
-
-        increaseQuantity = () =>
-        {
-            // this.state.qty += 1;
-            // console.log('this', this.state);
-
-            // setState form 1
-            // this.setState({
-            //     qty: this.state.qty + 1
-            // });
-
-            // setState form 2 - if prevState required use this
-            this.setState((prevState) => {
-                return {qty:prevState.qty + 1}
-            });
-        }
-
-        decreseQuantity = () => {
-            const {qty} = this.state;
-
-            if(qty === 0) {
-                return
-            }
-            this.setState((prevState) => {
-                return {
-                    qty: prevState.qty - 1
-                }
-            })
-        }
-    render()
-    {
-        console.log('this.props', this.props)
-        const {price, title, qty} = this.props.product;
-        return (
-            <div className = "cart-item">
-                <div className = "left-block"> <img alt="" style = {styles.image}/></div>
-                <div className = "right-block">
-                <div style = {{fontSize: 25 }}>{title}</div>
-                <div style = {{color: 'blue' }}>Rs {price}</div>
-                <div style = {{color: 'blue7' }}>Qty: {qty}</div>
-                </div>
-
-                <div className = "cart-item-actions">
-                    {/* Buttons */}
-                    <img alt = "increase" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/1237/1237946.png"  onClick = {this.increaseQuantity} />
-                    {/* onClick = {this.increaseQuantity} */}
-                    <img alt ="decrease" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/992/992683.png" onClick = {this.decreseQuantity}/>
-                    <img alt = "delete" className = "action-icons" src = "https://cdn-icons-png.flaticon.com/512/484/484611.png" />
-                </div>
-            </div>
-        )
-    }
+class CartItem extends React.Component {
+  render () {
+    console.log('this.props', this.props);
+    const { price, title, qty } = this.props.product;
+    const {product, onIncreaseQuantity, onDecreaseQuantity } = this.props;
+    return (
+      <div className="cart-item">
+        {this.props.jsx}
+        <div className="left-block">
+          <img style={styles.image} />
+        </div>
+        <div className="right-block">
+          <div style={ { fontSize: 25 } }>{title}</div>
+          <div style={ { color: '#777' } }>Rs {price} </div>
+          <div style={ { color: '#777' } }>Qty: {qty} </div>
+          <div className="cart-item-actions">
+            {/* Buttons */}
+            <img
+              alt="increase"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/992/992651.png"
+              onClick={() => onIncreaseQuantity(product)}
+            />
+            <img
+              alt="decrease"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/1828/1828899.png"
+              onClick={() => this.props.onDecreaseQuantity(this.props.product)}
+            />
+            <img
+              alt="delete"
+              className="action-icons"
+              src="https://cdn-icons-png.flaticon.com/128/484/484611.png"
+            />
+            onClick = {() => onDeleteProduct(product.id)}
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-    image: {
-        height: 110,
-        width: 110,
-        borderRadius: 4
-    }
+  image: {
+    height: 110,
+    width: 110,
+    borderRadius: 4,
+    background: '#ccc'
+  }
 }
 
 export default CartItem;
--- src/cart.js
+++ src/cart.js
@@ -1,32 +1,92 @@
 import React from 'react';
 import CartItem from './CartItems';
+
 class Cart extends React.Component {
-    render () {
-        // const arr = [1, 2, 3, 4, 5];
-        const { products } = this.state;
-        return(
-            <div className = "cart">
-                {/* <CartItem qty={1} price = {99} title = {"watch"} img = {''} /> */}
-                {products.map((product) =>{
-                    return(
-                        <CartItem
-                        product = {product}
-                        key = {product.id}
-                        // func = {() => console.log('sdsd')}
-                        // isloggedin = {false}
-                        // jsx = {<h1>Text</h1>}
-                        // comp = {<CartItem/>}
-                        />
-                    )
-                
-                })}
-                </div>);
-                // <CartItem/>
-                // <CartItem/>
-                {/* { arr.map((item) => {
-                    return item + 5;
-                })} */}
-            }
+  constructor () {
+    super();
+    this.state = {
+      products: [
+        {
+          price: 99,
+          title: 'Watch',
+          qty: 1,
+          img: '',
+          id: 1
+        },
+        {
+          price: 999,
+          title: 'Mobile Phone',
+          qty: 10,
+          img: '',
+          id: 2
+        },
+        {
+          price: 999,
+          title: 'Laptop',
+          qty: 4,
+          img: '',
+          id: 3
+        }
+      ]
     }
+    // this.increaseQuantity = this.increaseQuantity.bind(this);
+    // this.testing();
+  }
+  handleIncreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    products[index].qty += 1;
+
+    this.setState({
+      products
+    })
+  }
+
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if(products[index].qty === 0) {
+        return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id);  // [{}]
+
+    this.setState({
+        products: items
+    })
+  }
+  render () {
+    const { products } = this.state;
+    return (
+      <div className="cart">
+        {products.map((product) => {
+          return (
+            <CartItem
+              product={product}
+              key={product.id}
+              onIncreaseQuantity={this.handleIncreaseQuantity}
+              onDecreaseQuantity={this.handleDecreaseQuantity}
+              onDeleteProduct = {this.handleDeleteProduct}
+            />
+          )
+        })}
+      </div>
+    );
+  }
+}
 
 export default Cart;
